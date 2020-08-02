e20d3fe868db2dfacb2826e9024a582286c6fdff
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-08-01 22:25:42 -0400

    Completing SOLID

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { IUsersRepository } from "../../repositories/IUserRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 
 export class CreateUserUseCase {
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
     ) {}
 
     async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,19 @@ export class CreateUserUseCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        await this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from: {
+                name: 'Equipe do Meu App',
+                email: 'equipe@john.com'
+            },
+            subject: 'seja bem-vindo à plataforma',
+            body: '<p>Você já pode fazer login em nossa plataforma.</p>'
+
+        })
     }
 }
